d2cd000188e34e003fd23a71cf750d73d5276385
johnthedesigner <john@johnthedesigner.com>
2017-12-22 13:19:33 -0500

    Fix text editor styles and fixed a bug with trying to edit non-text layers

--- src/Routes/CaseStudies/components/layers/DragHandle.js
+++ src/Routes/CaseStudies/components/layers/DragHandle.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { DragSource } from 'react-dnd'
+import React from "react";
+import PropTypes from "prop-types";
+import { DragSource } from "react-dnd";
 
-import { scaleAllDimensions } from '../../artboardUtils'
+import { scaleAllDimensions } from "../../artboardUtils";
 
 const handleSource = {
   beginDrag(props, monitor, component) {
-    props.selectLayer(props.layer.id)
+    props.selectLayer(props.layer.id);
     return {
       layerId: props.layer.id
-    }
+    };
   }
-}
+};
 
 function collect(connect, monitor) {
   return {
@@ -22,57 +22,59 @@ function collect(connect, monitor) {
 
 class DragHandle extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       dimensions: {
         x: 0,
         y: 0,
         height: 0,
         width: 0,
-        rotation: 0,
+        rotation: 0
       }
-    }
-    this.handleClick = this.handleClick.bind(this)
-    this.handleDoubleClick = this.handleDoubleClick.bind(this)
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleDoubleClick = this.handleDoubleClick.bind(this);
   }
 
   componentDidMount() {
-    const { layer, scaleFactor } = this.props
+    const { layer, scaleFactor } = this.props;
     this.setState({
       dimensions: scaleAllDimensions(layer.dimensions, scaleFactor, true)
-    })
+    });
   }
 
   componentWillReceiveProps(nextProps) {
-    const { layer, scaleFactor } = nextProps
+    const { layer, scaleFactor } = nextProps;
     this.setState({
       dimensions: scaleAllDimensions(layer.dimensions, scaleFactor, true)
-    })
+    });
   }
 
   handleClick(e) {
-    e.stopPropagation()
-    this.props.selectLayer(this.props.layer.id)
+    e.stopPropagation();
+    this.props.selectLayer(this.props.layer.id);
   }
 
   handleDoubleClick(e) {
-    e.stopPropagation()
-    this.props.enableTextEditor(this.props.layer.id)
+    e.stopPropagation();
+    if (this.props.layer.type === "text") {
+      this.props.enableTextEditor(this.props.layer.id);
+    }
   }
 
   render() {
-    const { connectDragSource, isDragging } = this.props
-    const { x, y, width, height, rotation } = this.state.dimensions
+    const { connectDragSource, isDragging } = this.props;
+    const { x, y, width, height, rotation } = this.state.dimensions;
 
     const dragHandleStyles = {
-      position: 'absolute',
+      position: "absolute",
       top: y ? y : 0,
       left: x ? x : 0,
       width: width ? width : 0,
       height: height ? height : 0,
       transform: `rotate(${rotation}deg)`,
-      opacity: isDragging ? 0 : 1,
-    }
+      opacity: isDragging ? 0 : 1
+    };
 
     return connectDragSource(
       <div
@@ -80,14 +82,15 @@ class DragHandle extends React.Component {
         onClick={this.handleClick}
         onDoubleClick={this.handleDoubleClick}
         onDrag={this.handleDrag}
-        style={dragHandleStyles}/>
-    )
+        style={dragHandleStyles}
+      />
+    );
   }
 }
 
 DragHandle.propTypes = {
   connectDragSource: PropTypes.func.isRequired,
-  isDragging: PropTypes.bool.isRequired,
-}
+  isDragging: PropTypes.bool.isRequired
+};
 
-export default DragSource('DRAGGABLE', handleSource, collect)(DragHandle);
+export default DragSource("DRAGGABLE", handleSource, collect)(DragHandle);
