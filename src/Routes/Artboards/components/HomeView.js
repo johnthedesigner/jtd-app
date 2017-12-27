d99a29493143458fb243e41a999a196c72d7664a
johnthedesigner <john@johnthedesigner.com>
2017-12-27 00:51:25 -0500

    small styling updates to home item

--- src/Routes/Artboards/components/HomeView.js
+++ src/Routes/Artboards/components/HomeView.js
@@ -10,14 +10,20 @@ class HomeView extends React.Component {
       <div className={"home-view__wrapper"}>
         <div className="home-view__featured-case-study">
           <HomeBlock artboardId={"fake"} deselectLayers={deselectLayers}>
-            <h3>Capitalize on low hanging fruit</h3>
-            <p>
-              Bring to the table win-win survival strategies to ensure proactive
-              domination. At the end of the day, going forward, a new normal
-              that has evolved from generation X is on the runway heading
-              towards a streamlined cloud solution. User generated content in
-              real-time will have multiple touchpoints for offshoring.
-            </p>
+            <div className="home-item">
+              <h3 className="home-item__title">
+                Capitalize on low hanging fruit
+              </h3>
+              <p className="home-item__blurb">
+                Bring to the table win-win survival strategies to ensure
+                proactive domination. At the end of the day, going forward, a
+                new normal that has evolved from generation X is on the runway
+                heading towards a streamlined cloud solution. User generated
+                content in real-time will have multiple touchpoints for
+                offshoring.
+              </p>
+              <button className="home-item__cta">Check this out</button>
+            </div>
           </HomeBlock>
         </div>
       </div>
