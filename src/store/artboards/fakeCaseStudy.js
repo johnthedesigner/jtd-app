e28d7d1d8296dc8620ceb9a1d37ec1c9f330800e
johnthedesigner <john@johnthedesigner.com>
2017-12-27 22:37:11 -0500

    Updated new layer icons

--- src/store/artboards/fakeCaseStudy.js
+++ src/store/artboards/fakeCaseStudy.js
@@ -18,10 +18,7 @@ export default {
       order: 0,
       dimensions: { x: 40, y: 500, width: 920, height: 460, rotation: 0 },
       adjustments: {
-        blending: {
-          mode: "normal",
-          opacity: 1
-        },
+        blending: { mode: "normal", opacity: 1 },
         fill: {
           type: "gradient",
           gradient: {
@@ -41,14 +38,11 @@ export default {
       order: 1,
       dimensions: { x: 40, y: 40, width: 920, height: 460, rotation: 0 },
       adjustments: {
-        blending: {
-          mode: "normal",
-          opacity: 1
-        },
+        blending: { mode: "normal", opacity: 1 },
         fill: {
           type: "gradient",
           gradient: {
-            start: "rgb(187, 107, 217)",
+            start: "hsl(28.100000000000023, 100%, 85.9%)",
             end: "rgb(11, 231, 183)",
             angle: 0
           },
@@ -59,15 +53,40 @@ export default {
       id: "3c5117d0-e53e-11e7-95a2-f1bdc7305b17"
     },
     {
+      type: "rectangle",
+      title: "rectangle",
+      order: 2,
+      dimensions: { x: 200, y: 180, width: 600, height: 600, rotation: 0 },
+      adjustments: {
+        blending: { mode: "normal", opacity: 1 },
+        fill: {
+          type: "gradient",
+          gradient: {
+            start: "rgb(255, 255, 255)",
+            end: "rgb(200, 200, 200)",
+            angle: 0
+          }
+        },
+        stroke: { color: "rgb(140, 140, 140)", width: "0" }
+      },
+      id: "5d983ef0-eb4c-11e7-b49b-ef07192b71ef"
+    },
+    {
+      imageId: "test1",
+      type: "image",
+      title: "image",
+      order: 3,
+      dimensions: { x: 200, y: 179, width: 600, height: 600, rotation: 0 },
+      adjustments: { blending: { mode: "normal", opacity: 1 } },
+      id: "fafa0600-eb75-11e7-b49b-ef07192b71ef"
+    },
+    {
       type: "ellipse",
       title: "ellipse",
-      order: 2,
+      order: 4,
       dimensions: { x: 260, y: 250, width: 500, height: 500, rotation: 0 },
       adjustments: {
-        blending: {
-          mode: "normal",
-          opacity: 1
-        },
+        blending: { mode: "color", opacity: 1 },
         fill: {
           type: "gradient",
           color: null,
@@ -84,13 +103,10 @@ export default {
     {
       type: "ellipse",
       title: "ellipse",
-      order: 3,
+      order: 5,
       dimensions: { x: 240, y: 250, width: 500, height: 500, rotation: 0 },
       adjustments: {
-        blending: {
-          mode: "normal",
-          opacity: 1
-        },
+        blending: { mode: "multiply", opacity: 1 },
         fill: {
           type: "gradient",
           color: null,
@@ -105,16 +121,69 @@ export default {
       id: "624631f0-e53e-11e7-95a2-f1bdc7305b17"
     },
     {
+      type: "rectangle",
+      title: "rectangle",
+      order: 6,
+      dimensions: { x: 200, y: 178, width: 600, height: 48, rotation: 0 },
+      adjustments: {
+        blending: { mode: "normal", opacity: 1 },
+        fill: {
+          type: "color",
+          gradient: {
+            start: "rgb(255, 255, 255)",
+            end: "rgb(200, 200, 200)",
+            angle: 0
+          },
+          color: "rgb(200, 200, 200)"
+        },
+        stroke: { color: "rgb(140, 140, 140)", width: "0" }
+      },
+      id: "38aad0e6-e334-42c4-b2d5-699d523c482d"
+    },
+    {
+      type: "ellipse",
+      title: "ellipse",
+      order: 7,
+      dimensions: { x: 210, y: 188, width: 30, height: 30, rotation: 0 },
+      adjustments: {
+        blending: { mode: "normal", opacity: 1 },
+        fill: { type: "color", color: "rgb(255, 141, 192)" },
+        stroke: { color: "hsl(333.20000000000005, 90%, 62.1%)", width: "1" }
+      },
+      id: "76a75890-eb4c-11e7-b49b-ef07192b71ef"
+    },
+    {
+      type: "ellipse",
+      title: "ellipse",
+      order: 8,
+      dimensions: { x: 250, y: 188, width: 30, height: 30, rotation: 0 },
+      adjustments: {
+        blending: { mode: "normal", opacity: 1 },
+        fill: { type: "color", color: "rgb(255, 178, 110)" },
+        stroke: { color: "hsl(28.100000000000023, 90%, 57.3%)", width: "1" }
+      },
+      id: "8a2a622c-3843-4df4-9081-95b8d56808c3"
+    },
+    {
+      type: "ellipse",
+      title: "ellipse",
+      order: 9,
+      dimensions: { x: 290, y: 188, width: 30, height: 30, rotation: 0 },
+      adjustments: {
+        blending: { mode: "normal", opacity: 1 },
+        fill: { type: "color", color: "rgba(11,231,183,1)" },
+        stroke: { color: "hsl(166.89999999999998, 81.8%, 38%)", width: "1" }
+      },
+      id: "5e098bb6-f146-419a-b76f-f6dc1fb8e307"
+    },
+    {
       type: "text",
       title: "text",
       text: "Lorem ipsum dolor.",
-      order: 4,
+      order: 10,
       dimensions: { x: 100, y: 450, width: 800, height: 100, rotation: 0 },
       adjustments: {
-        blending: {
-          mode: "normal",
-          opacity: 1
-        },
+        blending: { mode: "normal", opacity: 1 },
         text: {
           align: "center",
           fontFamily: "mono",
@@ -134,5 +203,6 @@ export default {
   history: [],
   editingTextLayer: null,
   activeFlyout: null,
-  editableTextLayer: null
+  editableTextLayer: null,
+  showImagePicker: false
 };
