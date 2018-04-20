ac59e6404e8d443c377e30efa0dce8fbe826002e
johnthedesigner <john@johnthedesigner.com>
2018-04-19 20:25:16 -0400

    Changed monospace font to IBM Plex

--- src/components/artboards/adjustments/adjustmentOptions.js
+++ src/components/artboards/adjustments/adjustmentOptions.js
@@ -1,24 +1,40 @@
 export const typeStyles = {
   families: [
     {
-      id: 'sans',
-      name: 'Sans-serif',
-      value: '\'Source Sans Pro\', sans-serif',
-      weights: [ 300, 400, 700, 900 ],
+      id: "sans",
+      name: "Sans-serif",
+      value: "'Source Sans Pro', sans-serif",
+      weights: [300, 400, 700, 900],
       hasItalic: true
     },
     {
-      id: 'mono',
-      name: 'Monospace',
-      value: '\'Source Code Pro\', sans-serif',
-      weights: [ 300, 400 ],
+      id: "mono",
+      name: "Monospace",
+      value: "'IBM Plex Mono', sans-serif",
+      weights: [200, 400],
       hasItalic: false
     }
-  ]}
+  ]
+};
 
-export const fontSizes = [6,8,12,16,18,24,28,32,36,48,60,72,96,144]
+export const fontSizes = [
+  6,
+  8,
+  12,
+  16,
+  18,
+  24,
+  28,
+  32,
+  36,
+  48,
+  60,
+  72,
+  96,
+  144
+];
 
 export const fillTypes = [
-  {type: 'color', name: 'Color'},
-  {type: 'gradient', name: 'Gradient'}
-]
+  { type: "color", name: "Color" },
+  { type: "gradient", name: "Gradient" }
+];
