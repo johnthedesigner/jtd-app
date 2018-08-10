5d4d7e5498467e6a2990ecafe38ba7e88d768084
johnthedesigner <john@johnthedesigner.com>
2018-08-10 18:05:47 -0400

    homepage reorganization

--- src/views/home/HomeView.js
+++ src/views/home/HomeView.js
@@ -13,44 +13,17 @@ class HomeView extends React.Component {
       <ArtboardShortcutsContainer>
         <div className={"home-view__wrapper"}>
           <div className="home-view__featured-case-study">
-            <HomeBlock artboardId={"fake"} deselectLayers={deselectLayers}>
-              <div className="home-item">
-                <h4 className="home-item__category">Case Study</h4>
-                <h3 className="home-item__title">
-                  One design system to rule them all
-                </h3>
-                <p className="home-item__blurb">
-                  Our collection of eCommerce shoe brands had recently been
-                  bought by Wolverine Worldwide. Freshly installed in a new,
-                  senior role, I had to figure out how our small team of
-                  designer/developers could keep a portfolio of prominent
-                  eCommerce sites up-to-date, consistent and beautiful.
-                </p>
-                <Link to="/posts/fake">
-                  <button className="home-item__cta">{"Look at this!"}</button>
-                </Link>
-              </div>
-            </HomeBlock>
-
             <HomeBlock artboardId={"fake2"} deselectLayers={deselectLayers}>
               <div className="home-item">
-                <h4 className="home-item__category">Prototype</h4>
-                <h3 className="home-item__title">
-                  Capitalize on low hanging fruit
-                </h3>
+                <h3 className="home-item__title">John the designer</h3>
                 <p className="home-item__blurb">
-                  Bring to the table win-win survival strategies to ensure
-                  proactive domination. At the end of the day, going forward, a
-                  new normal that has evolved from generation X is on the runway
-                  heading towards a streamlined cloud solution. User generated
-                  content in real-time will have multiple touchpoints for
-                  offshoring.
+                  Hey, my name is John Livornese and I’m the head of product
+                  design at{" "}
+                  <a href="http://luminoso.com" target="_blank">
+                    Luminoso{" "}
+                  </a>
+                  in Cambridge, Massachusetts.
                 </p>
-                <Link to="/posts/fake2">
-                  <button className="home-item__cta">
-                    {"But wait... There's more!"}
-                  </button>
-                </Link>
               </div>
             </HomeBlock>
           </div>
